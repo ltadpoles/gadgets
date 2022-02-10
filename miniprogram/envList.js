6d8c1e03753c50443626be223008f11a4254e5de
tadpole <ltadpole@163.com>
2022-02-10 18:10:35 +0800

    初始化

--- miniprogram/envList.js
+++
@@ -1,6 +0,0 @@
-const envList = [{"envId":"cloud1-1gq58d7y4ff7510b","alias":"cloud1"}]
-const isMac = false
-module.exports = {
-    envList,
-    isMac
-}
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,142 +1,58 @@
-// index.js
-// const app = getApp()
-const { envList } = require('../../envList.js');
+const app = getApp()
 
 Page({
   data: {
-    showUploadTip: false,
-    powerList: [{
-      title: '云函数',
-      tip: '安全、免鉴权运行业务代码',
-      showItem: false,
-      item: [{
-        title: '获取OpenId',
-        page: 'getOpenId'
+    toolList: [{
+        id: 0,
+        name: '手机号码归属地查询',
+        url: '/pages/phone/index'
       },
-      //  {
-      //   title: '微信支付'
-      // },
-       {
-        title: '生成小程序码',
-        page: 'getMiniProgramCode'
+      {
+        id: 1,
+        name: '火车查询'
       },
-      // {
-      //   title: '发送订阅消息',
-      // }
+      {
+        id: 2,
+        name: '全国天气预报'
+      },
+      {
+        id: 3,
+        name: 'IP查询'
+      },
+      {
+        id: 4,
+        name: '省份证号码查询'
+      },
+      {
+        id: 5,
+        name: '星座运势'
+      },
+      {
+        id: 6,
+        name: '历史上的今天'
+      },
+      {
+        id: 7,
+        name: '成语大全'
+      },
+      {
+        id: 8,
+        name: '脑筋急转弯'
+      },
+      {
+        id: 9,
+        name: '绕口令'
+      },
+      {
+        id: 10,
+        name: '笑话大全'
+      }
     ]
-    }, {
-      title: '数据库',
-      tip: '安全稳定的文档型数据库',
-      showItem: false,
-      item: [{
-        title: '创建集合',
-        page: 'createCollection'
-      }, {
-        title: '更新记录',
-        page: 'updateRecord'
-      }, {
-        title: '查询记录',
-        page: 'selectRecord'
-      }, {
-        title: '聚合操作',
-        page: 'sumRecord'
-      }]
-    }, {
-      title: '云存储',
-      tip: '自带CDN加速文件存储',
-      showItem: false,
-      item: [{
-        title: '上传文件',
-        page: 'uploadFile'
-      }]
-    }, {
-      title: '云托管',
-      tip: '不限语言的全托管容器服务',
-      showItem: false,
-      item: [{
-        title: '部署服务',
-        page: 'deployService'
-      }]
-    }],
-    envList,
-    selectedEnv: envList[0],
-    haveCreateCollection: false
   },
 
-  onClickPowerInfo(e) {
-    const index = e.currentTarget.dataset.index;
-    const powerList = this.data.powerList;
-    powerList[index].showItem = !powerList[index].showItem;
-    if (powerList[index].title === '数据库' && !this.data.haveCreateCollection) {
-      this.onClickDatabase(powerList);
-    } else {
-      this.setData({
-        powerList
-      });
-    }
-  },
-
-  onChangeShowEnvChoose() {
-    wx.showActionSheet({
-      itemList: this.data.envList.map(i => i.alias),
-      success: (res) => {
-        this.onChangeSelectedEnv(res.tapIndex);
-      },
-      fail (res) {
-        console.log(res.errMsg);
-      }
-    });
-  },
-
-  onChangeSelectedEnv(index) {
-    if (this.data.selectedEnv.envId === this.data.envList[index].envId) {
-      return;
-    }
-    const powerList = this.data.powerList;
-    powerList.forEach(i => {
-      i.showItem = false;
-    });
-    this.setData({
-      selectedEnv: this.data.envList[index],
-      powerList,
-      haveCreateCollection: false
-    });
-  },
-
-  jumpPage(e) {
+  toolClick(e) {
     wx.navigateTo({
-      url: `/pages/${e.currentTarget.dataset.page}/index?envId=${this.data.selectedEnv.envId}`,
-    });
-  },
-
-  onClickDatabase(powerList) {
-    wx.showLoading({
-      title: '',
-    });
-    wx.cloud.callFunction({
-      name: 'quickstartFunctions',
-      config: {
-        env: this.data.selectedEnv.envId
-      },
-      data: {
-        type: 'createCollection'
-      }
-    }).then((resp) => {
-      if (resp.result.success) {
-        this.setData({
-          haveCreateCollection: true
-        });
-      }
-      this.setData({
-        powerList
-      });
-      wx.hideLoading();
-    }).catch((e) => {
-      console.log(e);
-      this.setData({
-        showUploadTip: true
-      });
-      wx.hideLoading();
-    });
+      url: e.currentTarget.dataset.item.url,
+    })
   }
 });
